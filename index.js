f77ced7e5696dae88be346995a3ecd284dacfa1b
Advait Deshpande <Advait.Deshpande@extentia.com>
2019-01-09 16:38:20 +0530

    first commit1

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname+'/index.html')
+});
+
+app.listen(port, () => { console.log(`App listening on port ${port}`) });
