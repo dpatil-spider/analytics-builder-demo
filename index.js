7141bb0cc6cfd1bd52895984457f65993f21b412
Advait Mangesh Deshpande <deshpandeadvait88@gmail.com>
2019-01-10 15:39:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use("/images", express.static(__dirname + '/images'));
 app.use("/vendor", express.static(__dirname + '/vendor'));
 app.use("/vendor", express.static(__dirname + '/js'));
 app.use("/css", express.static(__dirname + '/css'));
-
+app.use("/fonts", express.static(__dirname + '/fonts'));
 app.get('/', (req, res) => {
 	res.sendFile(__dirname+'/product.html')
 });
