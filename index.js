e9055f444ae860297973b88e964bbd4d5cde6ad7
Advait Mangesh Deshpande <deshpandeadvait88@gmail.com>
2019-01-10 15:31:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 var port = process.env.PORT || 3000;
 app.use("/images", express.static(__dirname + '/images'));
 app.use("/vendor", express.static(__dirname + '/vendor'));
+app.use("/vendor", express.static(__dirname + '/js'));
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname+'/product.html')
